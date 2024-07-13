c00072d43eb61b6f59054539cb0d5e00441c3c2f
mayankpathak870 <mayankpathak870@gmail.com>
2024-07-13 22:50:11 +0530

    Smart contact manager

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  /** below entry has been added for identifing the .htmp and .js files */
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  darkMode: 'selector',
+}
+
